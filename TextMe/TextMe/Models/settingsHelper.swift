7c58275a08a48dac3a178d4c9764b87bd1771c82
ofirsharabi <ofirsharabi00@walla.com>
2019-04-23 23:12:16 +0300

    Current updated app wih settings and contacts

--- TextMe/TextMe/Models/settingsHelper.swift
+++ TextMe/TextMe/Models/settingsHelper.swift
@@ -15,9 +15,17 @@ class settingsHelper : NSObject, UICollectionViewDataSource,UICollectionViewDele
     
     
     let collectionView : UICollectionView = {
-      let layout = UICollectionViewFlowLayout()
+        let layout = UICollectionViewFlowLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         return cv
+    }()
+    
+    let cellHeight: CGFloat = 50
+    
+    var settings: [Setting] = {
+        return [Setting(name: "Settings", imageName: "icons8-settings_filled-1"),
+                Setting(name: "Profile",imageName: "icons8-user_filled"),
+                Setting(name: "Logout",imageName: "icons8-logout_rounded_up_filled")]
     }()
     
     func handleBackgroundBlur(){
@@ -32,11 +40,12 @@ class settingsHelper : NSObject, UICollectionViewDataSource,UICollectionViewDele
             
             window.addSubview(collectionView)
             
-            let height: CGFloat = 200
+            
+            let height: CGFloat = CGFloat(settings.count)  * cellHeight + 15
             let y = window.frame.height - height
             
             collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
-            collectionView.backgroundColor = UIColor.primaryDarkColor
+            collectionView.backgroundColor = UIColor.white
             
             blackView.frame = window.frame
             blackView.alpha = 0
@@ -45,7 +54,7 @@ class settingsHelper : NSObject, UICollectionViewDataSource,UICollectionViewDele
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                 self.blackView.alpha = 1
                 
-                  self.collectionView.frame = CGRect(x: 0, y: y, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+                self.collectionView.frame = CGRect(x: 0, y: y, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
             }, completion: nil)
         }
     }
@@ -62,20 +71,38 @@ class settingsHelper : NSObject, UICollectionViewDataSource,UICollectionViewDele
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return settings.count
     }
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "settingsCell", for: indexPath)
-            
-            return cell
-        }
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "settingsCell", for: indexPath) as! SettingsCell
+        
+        let setting = settings[indexPath.item]
+        cell.setting = setting
+        
+        return cell
+    }
+    
+    
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: 50)
+        return CGSize(width: collectionView.frame.width, height: cellHeight)
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        
+        
+        
+        
+        
+    }
+    
     
     override init() {
         super.init()
