f0bdd9802bc02954ce5d241560646329803e6fc8
ofirsharabi <ofirsharabi00@walla.com>
2019-04-26 19:26:18 +0300

    Fixed Friends and

--- TextMe/TextMe/Controllers/addFriendsViewController.swift
+++ TextMe/TextMe/Controllers/addFriendsViewController.swift
@@ -20,6 +20,7 @@ UITableViewDataSource, UISearchBarDelegate{
     
     var usersArray = [User]()
     var filterdUsers = [User]()
+    var friends = [User]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +32,7 @@ UITableViewDataSource, UISearchBarDelegate{
         
         contactsTableView.register(userCell.self, forCellReuseIdentifier: "cell")
         fetchUser()
+        
 
         // Do any additional setup after loading the view.
     }
@@ -80,6 +82,14 @@ UITableViewDataSource, UISearchBarDelegate{
         cell.textLabel?.text = user.displayName
         cell.detailTextLabel?.text = user.Email
         
+        // Check if User in Friends Already
+        for id in friends {
+            if user.id == id.id {
+                // Disable add friend
+                cell.cellButton.isEnabled = false
+                cell.cellButton.alpha = 0
+            }
+        }
         
         return cell
     }
@@ -109,8 +119,23 @@ UITableViewDataSource, UISearchBarDelegate{
         // Delegate for Add Friend Btn
   
        @objc func onAddClicked(_ sender: UIButton) {
+            // Activating Delegate Method
             delegate?.addClicked(user: user)
+            // Disabling Button
+            sender.isEnabled = false
+        // Animation
+        UIView.transition(with: sender, duration: 3, options: .curveEaseOut, animations: {
+            sender.setImage(UIImage(named: "icons8-checkmark"), for: .normal)
+        }) { (finished) in
+            if finished {
+                DispatchQueue.asyncAfter(deadline:.now() + 1.0,execute: {
+                    UIView.animate(withDuration: 1.5, animations: {
+                        sender.alpha = 0
+                    })
+                })
+            }
         }
+    }
         
         required init?(coder aDecoder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
@@ -201,7 +226,7 @@ extension addFriendsViewController : cellAddDelegate {
         let userId = Auth.auth().currentUser?.uid
         let dbRef = Database.database().reference().child("Users").child(userId!)
         
-        let userDict = ["id": user.id]
+        let userDict = ["uid": user.id]
         dbRef.child("Friends").childByAutoId().setValue(userDict) { (error, dbRef) in
             if(error != nil){
                 print(error)
